abc0439574736cea073f12aa730c784a99f82cfb
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-08 17:48:14 +0530

    first commit

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
+const CACHE = "Data";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = [`https://jsonplaceholder.typicode.com/users`];
@@ -24,24 +24,54 @@ if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
+
+self.addEventListener("fetch", (event) => {
+    if(!navigator.onLine){
+        event.respondWith(
+          caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+              return cachedResponse;
+            }
+
+            // If the request is not in cache, fetch it from the network
+            return fetch(event.request)
+              .then((response) => {
+                // Clone the response as it can only be consumed once
+                const responseClone = response.clone();
+
+                // Cache the response for future requests
+                caches.open(CACHE).then((cache) => {
+                  cache.put(event.request, responseClone);
+                });
+
+                return response;
+              })
+              .catch((error) => {
+                console.log(error)
+            //   return  caches.match(event.request)
+              });
+          })
+        );
+    }
+  });
