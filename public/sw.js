ec223dffe83011b23ad3caee49567aaee4794358
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-08 17:26:38 +0530

    first commit

--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,11 @@
-// // This is the "Offline page" service worker
-
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-// const CACHE = "pwabuilder-page";
-
-// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-// const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-// self.addEventListener("message", (event) => {
-//   if (event.data && event.data.type === "SKIP_WAITING") {
-//     self.skipWaiting();
-//   }
-// });
-
-// self.addEventListener('install', async (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE)
-//       .then((cache) => cache.add(offlineFallbackPage))
-//   );
-// });
-
-// if (workbox.navigationPreload.isSupported()) {
-//   workbox.navigationPreload.enable();
-// }
-
-// self.addEventListener('fetch', (event) => {
-//   if (event.request.mode === 'navigate') {
-//     event.respondWith((async () => {
-//       try {
-//         const preloadResp = await event.preloadResponse;
-
-//         if (preloadResp) {
-//           return preloadResp;
-//         }
-
-//         const networkResp = await fetch(event.request);
-//         return networkResp;
-//       } catch (error) {
-
-//         const cache = await caches.open(CACHE);
-//         const cachedResp = await cache.match(offlineFallbackPage);
-//         return cachedResp;
-//       }
-//     })());
-//   }
-// });
-
+// This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE_NAME_API = "api-cache";
-const CACHE_NAME_STATIC = "static-cache";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = [`**/*.{html}`];
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -61,18 +15,15 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME_STATIC)
-      .then((cache) => cache.addAll([
-        // Add static files like HTML, CSS, and JavaScript here
-        offlineFallbackPage,
-        // Add other static assets
-      ]))
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
 if (workbox.navigationPreload.isSupported()) {
   workbox.navigationPreload.enable();
 }
+
 self.addEventListener('fetch', (event) => {
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
@@ -84,24 +35,13 @@ self.addEventListener('fetch', (event) => {
         }
 
         const networkResp = await fetch(event.request);
-
-        // Cache API responses if they are successful and the URL matches '/users'
-        if (event.request.url.includes('/users') && networkResp.status === 200) {
-          const cache = await caches.open(CACHE_NAME_API);
-          cache.put(event.request, networkResp.clone());
-        }
-
         return networkResp;
       } catch (error) {
-        const cache = await caches.open(CACHE_NAME_STATIC);
+
+        const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
       }
     })());
-  } else {
-    // Handle non-navigate requests, e.g., for images, fonts, etc.
-    // You can add caching logic for other types of requests here.
-    // By default, they will be served from the browser's cache.
   }
 });
-
