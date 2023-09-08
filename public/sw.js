1b5f6c1c2be2a31bddba2894a0350de73120cd8d
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-08 18:47:40 +0530

    first

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "Data";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = [`/users`];
+const offlineFallbackPage = [`/`];
 
 
 self.addEventListener("message", (event) => {
