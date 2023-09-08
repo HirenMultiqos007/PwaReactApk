4b47e2b95e726e123873710df017509f1899bb80
HirenMultiqos007 <hirenb.multiqos@gmail.com>
2023-09-08 17:21:32 +0530

    first commit

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,56 @@
-// This is the "Offline page" service worker
+// // This is the "Offline page" service worker
+
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+// const CACHE = "pwabuilder-page";
+
+// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+// const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
+
+// self.addEventListener('install', async (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//   );
+// });
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
+
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
+
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
+
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
+
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
+
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const CACHE_NAME_API = "api-cache";
+const CACHE_NAME_STATIC = "static-cache";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 self.addEventListener("message", (event) => {
@@ -15,8 +61,12 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+    caches.open(CACHE_NAME_STATIC)
+      .then((cache) => cache.addAll([
+        // Add static files like HTML, CSS, and JavaScript here
+        offlineFallbackPage,
+        // Add other static assets
+      ]))
   );
 });
 
@@ -35,13 +85,23 @@ self.addEventListener('fetch', (event) => {
         }
 
         const networkResp = await fetch(event.request);
+
+        // Cache API responses if they are successful
+        if (event.request.url.includes("/users") && networkResp.status === 200) {
+          const cache = await caches.open(CACHE_NAME_API);
+          cache.put(event.request, networkResp.clone());
+        }
+
         return networkResp;
       } catch (error) {
-
-        const cache = await caches.open(CACHE);
+        const cache = await caches.open(CACHE_NAME_STATIC);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
       }
     })());
+  } else {
+    // Handle non-navigate requests, e.g., for images, fonts, etc.
+    // You can add caching logic for other types of requests here.
+    // By default, they will be served from the browser's cache.
   }
 });
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Alert, Table } from "react-bootstrap";
 const About = () => {
+  const [data, setData] = useState([]);
+  const [mode, setMode] = useState("online");
+  useEffect(() => {
+    let url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result, "result");
+        setData(result);
+        // localStorage.setItem("users", JSON.stringify(result));
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+        let finaldata = localStorage.getItem("users");
+        setData(JSON.parse(finaldata));
+        setMode("offline");
+      });
+  }, []);
+
   return (
     <div>
-       <h1>About</h1>
+    <div>
+      {mode === "offline" && (
+        <div>
+          <Alert variant="warning">
+            <Alert.Heading>You are in Offline mode</Alert.Heading>
+          </Alert>
+        </div>
+      )}
     </div>
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th> Id </th>
+          <th> Name </th>
+          <th> Email </th>
+          <th>Address</th>
+          <th>Hiren</th>
+
+        </tr>
+      </thead>
+      <tbody>
+        {data?.map((item) => {
+          return (
+            <tr>
+              <td>{item?.id}</td>
+              <td>{item?.name}</td>
+              <td>{item?.email}</td>
+              <td>{item?.address?.street}</td>
+              <td> Helloo </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  </div>
   )
 }
 
